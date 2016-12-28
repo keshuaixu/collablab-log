f59ec954f26eb1eeeac4bcedba04577de1e20099
Cosine <urillx@gmail.com>
2016-12-28 15:23:15 -0500

    log friendly human names

--- collablab-log.py
+++ collablab-log.py
@@ -1,31 +1,42 @@
 from influxdb import InfluxDBClient
 import requests
 import logging
-from credentials import influxdb_credentials
+from credentials import influxdb_credentials, redis_credentials
+import redis
 
 logging.basicConfig(filename='error.log', format='%(asctime)s %(levelname)s %(message)s')
 logging.getLogger().addHandler(logging.StreamHandler())
 
 db = InfluxDBClient(**influxdb_credentials)
+red = redis.Redis(**redis_credentials)
 
 try:
     r = requests.get('http://collablab.wpi.edu/lab/status').json()
-    lab_open = r['open']
-    members = dict.fromkeys(r['members'], 1)
-    json_body = [
-        {
-            'measurement': 'head_count',
-            'fields': {'value': len(members)}
-        }
-    ]
-    if len(members) > 0:
-        json_body.append(
+
+    try:
+        red.hmset('collablab_names', r['members'])
+    except Exception as e:
+        logging.exception('redis fucked')
+
+    try:
+        lab_open = r['open']
+        members = dict.fromkeys(r['members'], 1)
+        json_body = [
             {
-                'measurement': 'occupants',
-                'fields': members
+                'measurement': 'head_count',
+                'fields': {'value': len(members)}
             }
-        )
-    db.write_points(json_body)
+        ]
+        if len(members) > 0:
+            json_body.append(
+                {
+                    'measurement': 'occupants',
+                    'fields': members
+                }
+            )
+        db.write_points(json_body)
+    except Exception as e:
+        logging.exception('influxdb fucked')
 
 except Exception as e:
-    logging.exception('bork')
+    logging.exception('collablab website fucked')
